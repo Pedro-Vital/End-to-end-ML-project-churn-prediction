f57b692c60406dd142e4331d3fb2be302b0d2fe2
Pedro Lucas Salazar Vital <pedrosalazarvital@gmail.com>
2025-12-09 14:54:43 -0400

    fix: delay added to prevent prefect handler of creating log file

--- src/churn_project/logger.py
+++ src/churn_project/logger.py
@@ -1,24 +1,24 @@
 import logging
 import os
-import sys
 from datetime import datetime
 
 LOG_DIR = "logs"
-
-# Create log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
-LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
-
 os.makedirs(LOG_DIR, exist_ok=True)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]",
-    handlers=[
-        logging.FileHandler(LOG_PATH),
-        logging.StreamHandler(sys.stdout),  # So we can see the log in the terminal
-    ],
-)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logger = logging.getLogger("mlProjectLogger")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+if not logger.handlers:
+    # delay=True prevents the handler from creating the file until first write
+    file_handler = logging.FileHandler(LOG_PATH, delay=True)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
